d95dec5b8cef42983b74e9dacd588e0236f63a6f
Jose Luis Gutierrez <josluigtz@gmail.com>
2018-12-29 11:00:36 -0600

    primero de hoy

--- sw.js
+++ sw.js
@@ -1,17 +1,22 @@
 // imports
+importScripts('https://cdn.jsdelivr.net/npm/pouchdb@7.0.0/dist/pouchdb.min.js')
+
+importScripts('views/js/sw-db.js');
 importScripts('views/js/sw-utils.js');
+
+
 
 const STATIC_CACHE    = 'static-v1';
 const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-  //  '/',
+    '/',
     'index.html',
-    'css/bootstrap.css',
+    'views/css/bootstrap.css',
     'php.ini',
-    'favicon.ico',
-    'index.php',
+    'favicon.png',
+    'contenido.php',
     'models/gestorSlide.php',
     'models/gestorArticulos.php',
     'models/gestorMensajes.php',
@@ -41,6 +46,10 @@ const APP_SHELL = [
     'views/modules/videos.php',  
     'views/js/app.js', 
     'views/js/sw-utils.js',
+    'views/js/sw-db.js',   
+    'views/js/camara-class.js',    
+    'views/js/script.js',    
+    'views/js/sitio.js',   
     
 ];
 
@@ -58,11 +67,13 @@ const APP_SHELL_INMUTABLE = [
     'views/js/jquery.scrollUp.js',
     'views/js/sweetalert.min.js',
     'views/js/script.min.js',
-   
+    'views/js/tether.min.js',    
+    'views/js/jquery.min.js',
+    'views/js/owl.carousel.js',   
 ];
 
-self.addEventListener('install', e => {
 
+self.addEventListener('install', e => {
 
     const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
         cache.addAll( APP_SHELL ));
@@ -70,11 +81,10 @@ self.addEventListener('install', e => {
     const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
         cache.addAll( APP_SHELL_INMUTABLE ));
 
-
-
     e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
 
 });
+
 
 self.addEventListener('activate', e => {
 
@@ -93,34 +103,146 @@ self.addEventListener('activate', e => {
         });
 
     });
+
     e.waitUntil( respuesta );
 
 });
 
 
-
 self.addEventListener( 'fetch', e => {
 
+    let respuesta;
 
-    const respuesta = caches.match( e.request ).then( res => {
+    if ( e.request.url.includes('/api') ) {
 
-        if ( res ) {
-            return res;
-        } else {
+        // return respuesta????
+        respuesta = manejoApiMensajes( DYNAMIC_CACHE, e.request );
 
-            return fetch( e.request ).then( newRes => {
+    } else {
 
-                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+        respuesta = caches.match( e.request ).then( res => {
 
-            });
+            if ( res ) {
+                
+                actualizaCacheStatico( STATIC_CACHE, e.request, APP_SHELL_INMUTABLE );
+                return res;
+                
+            } else {
+    
+                return fetch( e.request ).then( newRes => {
+    
+                    return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+    
+                });
+    
+            }
+    
+        });
 
-        }
-
-    });
-
-
+    }
 
     e.respondWith( respuesta );
 
 });
 
+
+// tareas asíncronas
+self.addEventListener('sync', e => {
+
+    console.log('SW: Sync');
+
+    if ( e.tag === 'nuevo-post' ) {
+
+        // postear a BD cuando hay conexión
+        const respuesta = postearMensajes();
+        
+        e.waitUntil( respuesta );
+    }
+
+});
+
+// Escuchar PUSH
+self.addEventListener('push', e => {
+
+    // console.log(e);
+
+    const data = JSON.parse( e.data.text() );
+
+    // console.log(data);
+
+
+    const title = data.titulo;
+    const options = {
+        body: data.cuerpo,
+        // icon: 'img/icons/icon-72x72.png',
+        icon: `img/avatars/${ data.usuario }.jpg`,
+        badge: 'img/favicon.ico',
+        image: 'https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/5/5b/Torre_de_los_Avengers.png/revision/latest?cb=20150626220613&path-prefix=es',
+        vibrate: [125,75,125,275,200,275,125,75,125,275,200,600,200,600],
+        openUrl: '/',
+        data: {
+            // url: 'https://google.com',
+            url: '/',
+            id: data.usuario
+        },
+        actions: [
+            {
+                action: 'thor-action',
+                title: 'Thor',
+                icon: 'img/avatar/thor.jpg'
+            },
+            {
+                action: 'ironman-action',
+                title: 'Ironman',
+                icon: 'img/avatar/ironman.jpg'
+            }
+        ]
+    };
+
+
+    e.waitUntil( self.registration.showNotification( title, options) );
+
+
+});
+
+
+// Cierra la notificacion
+self.addEventListener('notificationclose', e => {
+    console.log('Notificación cerrada', e);
+});
+
+
+self.addEventListener('notificationclick', e => {
+
+
+    const notificacion = e.notification;
+    const accion = e.action;
+
+
+    console.log({ notificacion, accion });
+    // console.log(notificacion);
+    // console.log(accion);
+    
+
+    const respuesta = clients.matchAll()
+    .then( clientes => {
+
+        let cliente = clientes.find( c => {
+            return c.visibilityState === 'visible';
+        });
+
+        if ( cliente !== undefined ) {
+            cliente.navigate( notificacion.data.url );
+            cliente.focus();
+        } else {
+            clients.openWindow( notificacion.data.url );
+        }
+
+        return notificacion.close();
+
+    });
+
+    e.waitUntil( respuesta );
+
+
+});
