c830dc71e1414e81aa83eb4761e50ca2a3f565b2
Jose Luis Gutierrez <josluigtz@gmail.com>
2018-12-31 11:20:03 -0600

    31-3

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const DYNAMIC_CACHE   = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+ //   '/',
     'index.html',
     'views/css/bootstrap.css',
     'images/icono.png',
